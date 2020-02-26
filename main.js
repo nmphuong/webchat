719f1e4febb4ce130260a8ef3c134e166c6e29cd
nmphuong <56857408+nmphuong@users.noreply.github.com>
2020-02-26 11:29:35 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 const socket = io('https://webrtcv1demo.herokuapp.com/');
 $('#divChat').hide();
+//$('#phuong').hide();
 socket.on('List_Online', arrUserInfo => {
-        $('#divChat').show();
+
+        //$('#phuong').show();
         $('#divRegister').hide();
+        //$('#phuong').click(() => {
+        $('#divChat').show();
+        //$('#phuong').hide();
+        //});
         arrUserInfo.forEach(user => {
                 const { ten, peerId } = user;
                 $('#divUlUser').append(`<button type="button" class="buttonUser" id="${peerId}">${ten}</button>`);
                 var my_peer = $('#my-peer').val();
+                PeerIdBoss = my_peer;
                 $(`#${my_peer}`).css("background-color", "#ff8490");
                 $(`#${my_peer}`).prop('disabled', true);
         });
         socket.on('have_a_register', user => {
+                //$('#phuong').click(() => {
                 const { ten, peerId } = user;
                 $('#divUlUser').append(`<button type="button" class="buttonUser" id="${peerId}">${ten}</button>`);
+                //});
+                // $('#refresh').click(() => {
+                //         const { ten, peerId } = user;
+                //         $('#divUlUser').append(`<button type="button" class="buttonUser" id="${peerId}">${ten}</button>`);
+                // });
         });
         socket.on('user_disconnect', peerId => {
                 $(`#${peerId}`).remove();
@@ -58,19 +71,16 @@ var offer = 1;
 var count_video = 1;
 $('#ulUser').on('click', 'button', function () {
         const id = $(this).attr('id');
-        $('#divUserScreen').append(`<div class="col-lg-4 div${id}"><video id="remoteStream${count_video}" class="w-100 video${id}" controls></video><br class="br${id}"></div>`);
-        
-
-        console.log(id);
+        $('#divUserScreen').append(`<div class="col-lg-4 col-4 div${id}"><video id="remoteStream${count_video}" class="w-100 video${id}" controls></video><br class="br${id}"></div>`);
 
         let remote = "remoteStream" + count_video;
-                openStream()
-                        .then(stream => {
-                                playStream('localStream', stream);
-                                peer.call(id, stream).on('stream', remoteStream => playStream(remote, remoteStream));
-                        });
-                        $(`#${id}`).prop('disabled', true);
-                        $(`#${id}`).css("background-color", "#acacac");
+        openStream()
+                .then(stream => {
+                        playStream('localStream', stream);
+                        peer.call(id, stream).on('stream', remoteStream => playStream(remote, remoteStream));
+                });
+        $(`#${id}`).prop('disabled', true);
+        $(`#${id}`).css("background-color", "#acacac");
         offer = 2;
         count_video += 1;
 });
@@ -80,27 +90,14 @@ peer.on('call', call => {
         $('#remove').remove();
         $('#remoteStream1').remove();
         $('#br').remove();
-        $('#divUserScreen').append(`<div class="col-lg-4" id="remove"><video id="remoteStream1" class="w-100" controls></video><br id="br"></div>`);
-        if (answer == 1) {
-                $('.buttonUser').prop('disabled', true);
-                $('.buttonUser').css("background-color", "#acacac");
-                openStream()
-                        .then(stream => {
-                                playStream('remoteStream1', stream);
-                                call.answer(stream);
-                                call.on('stream', remoteStream => playStream('localStream', remoteStream));
-                        });
-        } else {
-                $('.buttonUser').prop('disabled', true);
-                $('.buttonUser').css("background-color", "#acacac");
-                openStream()
-                        .then(stream => {
-                                playStream('remoteStream1', stream);
-                                call.answer(stream);
-                                call1.answer(stream);
-                                call1.on('stream', remoteStream1 => playStream('localStream', remoteStream1));
-                                call.on('stream', remoteStream => playStream('localStream', remoteStream));
-                        });
-        }
+        $('#divUserScreen').append(`<div class="col-lg-4 col-4" id="remove"><video id="remoteStream${answer}" class="w-100" controls></video><br id="br"></div>`);
+        $('.buttonUser').prop('disabled', true);
+        $('.buttonUser').css("background-color", "#acacac");
+        openStream()
+                .then(stream => {
+                        playStream('remoteStream1', stream);
+                        call.answer(stream);
+                        call.on('stream', remoteStream => playStream('localStream', remoteStream));
+                });
         answer = 2;
 });
